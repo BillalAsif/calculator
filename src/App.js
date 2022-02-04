b2e9762af5bb35c6c2802cff61403a2b1a0921ec
Billal Asif <asif.billal@outlook.com>
2022-02-04 21:41:40 +0000

    css updated

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
     <div className='calculator-grid'>
 
       <div className='output'>
-        <div className='previous-operand'></div>
-        <div className='current-operand'></div>
+        <div className='previous-operand'>12312</div>
+        <div className='current-operand'>12312</div>
       </div>
 
       <button className='span-two'>AC</button>
